691b0ef86b67fa13b52215b0407dff37c9f858a2
PavelSteep <141700339+PavelSteep@users.noreply.github.com>
2025-03-09 16:36:43 +0100

    9.3-1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,27 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
+  eslintPluginPrettier,
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {globals: globals.node},
+    rules: {
+      'no-unused-vars': ['error', {args: 'none'}],
+      'no-undef': 'error',
+    },
+  },
 ];
+
+
+// import globals from "globals";
+// import pluginJs from "@eslint/js";
+
+
+// /** @type {import('eslint').Linter.Config[]} */
+// export default [
+//   {languageOptions: { globals: globals.node }},
+//   pluginJs.configs.recommended,
+// ];
