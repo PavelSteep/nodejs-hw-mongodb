e94eddbfbd6cefb18da3d229fa30b1b864f9d573
PavelSteep <141700339+PavelSteep@users.noreply.github.com>
2025-03-22 20:08:24 +0100

    22.03.25-2

--- src/db/models/models.js
+++ src/db/models/models.js
@@ -15,7 +15,7 @@ const contactsSchema = new Schema({
     required: false,
     match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, // Валидация формата email
   },
-  isFavorite: {
+  isFavourite: {
     type: Boolean,
     default: false,
   },
