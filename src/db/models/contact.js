148eb4d96e9b2bb265422f26c6519723590fdcc2
PavelSteep <141700339+PavelSteep@users.noreply.github.com>
2025-04-11 20:25:51 +0200

    11.04.25

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -1,13 +1,36 @@
 import { model, Schema } from 'mongoose';
 
-const contactSchema = new Schema({
-  name: { type: String, required: true },
-  age: { type: Number, required: true },
-  gender: { type: String, required: true, enum: ['male', 'female', 'other'] },
-  avgMark: { type: Number, required: true },
-  onDuty: { type: Boolean, default: false, required: true },
-  }, 
-  { timestamps: true, versionKey: false }
+const contactSchema = new Schema(
+  {
+    name: { 
+      type: String, 
+      required: false,
+      match: /^[A-Za-z\s]+$/,
+    },
+    phoneNumber: { 
+      type: String, 
+      required: false,
+      match: /^[+]*[0-9]{1,4}[ ]?([0-9]{1,4}[ ])?([0-9]{1,4}[ ])?([0-9]{4})$/,
+    },
+    email: { 
+      type: String, 
+      required: false, 
+      match: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+    },
+    isFavourite: { 
+      type: Boolean, 
+      required: false 
+    },
+    contactType: { 
+      type: String, 
+      required: false, 
+      enum: ['personal', 'business', 'other'],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
 );
 
 export const ContactCollection = model('contacts', contactSchema);
