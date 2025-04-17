8fd49b75da7fc97e591867b224844cae56b958da
PavelSteep <141700339+PavelSteep@users.noreply.github.com>
2025-04-17 11:47:10 +0200

    17.04.25

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -12,17 +12,39 @@ const contactsSchema = new Schema(
     },
     avgMark: { type: Number, required: true },
     onDuty: { type: Boolean, default: false, required: true },
-    phoneNumber: { type: String },
-    email: { type: String },
-    isFavourite: { type: Boolean },
-    contactType: { 
-      type: String, 
-      enum: ['personal', 'business', 'other'],
-    },
-    minAge: { type: Number },
   },
   { timestamps: true, versionKey: false },
 );
 
 export const ContactCollection = model('contacts', contactsSchema);
 export default ContactCollection;
+
+
+// import { model, Schema } from 'mongoose';
+// import { GENDERS } from '../../constants/gender.js';
+
+// const contactsSchema = new Schema(
+//   {
+//     name: { type: String, required: true },
+//     age: { type: Number, required: true },
+//     gender: { 
+//       type: String, 
+//       required: true, 
+//       enum: Object.values(GENDERS),
+//     },
+//     avgMark: { type: Number, required: true },
+//     onDuty: { type: Boolean, default: false, required: true },
+//     phoneNumber: { type: String },
+//     email: { type: String },
+//     isFavourite: { type: Boolean },
+//     contactType: { 
+//       type: String, 
+//       enum: ['personal', 'business', 'other'],
+//     },
+//     minAge: { type: Number },
+//   },
+//   { timestamps: true, versionKey: false },
+// );
+
+// export const ContactCollection = model('contacts', contactsSchema);
+// export default ContactCollection;
--- src/validation/updateContactValidationSchema.js
+++ src/validation/updateContactValidationSchema.js
@@ -1,17 +1,32 @@
 import Joi from 'joi';
 import { GENDERS } from '../constants/gender.js';
 
-export const updateContactValidationSchema = Joi.object().keys({
+export const updateContactValidationSchema = Joi.object({
   name: Joi.string().min(3).max(40),
   age: Joi.number().integer().min(6).max(120),
   gender: Joi.string().valid(...Object.values(GENDERS)),
   avgMark: Joi.number().min(1).max(12),
   onDuty: Joi.boolean(),
-  phoneNumber: Joi.string().min(7).max(20),
-  email: Joi.string().email(),
-  isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('personal', 'business', 'other'),
-  minAge: Joi.number().min(0),
 });
 
 export default updateContactValidationSchema;
+
+
+
+// import Joi from 'joi';
+// import { GENDERS } from '../constants/gender.js';
+
+// export const updateContactValidationSchema = Joi.object().keys({
+//   name: Joi.string().min(3).max(40),
+//   age: Joi.number().integer().min(6).max(120),
+//   gender: Joi.string().valid(...Object.values(GENDERS)),
+//   avgMark: Joi.number().min(1).max(12),
+//   onDuty: Joi.boolean(),
+//   phoneNumber: Joi.string().min(7).max(20),
+//   email: Joi.string().email(),
+//   isFavourite: Joi.boolean(),
+//   contactType: Joi.string().valid('personal', 'business', 'other'),
+//   minAge: Joi.number().min(0),
+// });
+
+// export default updateContactValidationSchema;
