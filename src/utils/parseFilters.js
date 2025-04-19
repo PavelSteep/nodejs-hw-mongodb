0784c93a7904f84abd3cb08419ea43ab6698f89f
PavelSteep <141700339+PavelSteep@users.noreply.github.com>
2025-04-19 16:29:12 +0200

    19.04.25-2

--- src/utils/parseFilters.js
+++ src/utils/parseFilters.js
@@ -1,16 +1,36 @@
-import { parseGender } from './parseGender.js';
-import { parseBoolean } from './parseBoolean.js';
-import { parseNumber } from './parseNumber.js';
 import { GENDERS } from '../constants/gender.js';
 
 export const parseFilters = (filter = {}) => {
-  const parsed = {
-    name: filter.name || '',
-    phoneNumber: filter.phoneNumber || '',
-    email: filter.email || '',
-    isFavourite: filter.isFavourite !== undefined ? Boolean(filter.isFavourite) : false, // если не передано, будет false
-    contactType: filter.contactType && GENDERS.includes(filter.contactType) ? filter.contactType : GENDERS[0],
-  };
+  const parsed = {};
+
+  if (filter.name) parsed.name = filter.name;
+  if (filter.phoneNumber) parsed.phoneNumber = filter.phoneNumber;
+  if (filter.email) parsed.email = filter.email;
+  if (filter.isFavourite !== undefined) parsed.isFavourite = filter.isFavourite === 'true' || filter.isFavourite === true;
+  if (filter.contactType && GENDERS.includes(filter.contactType)) {
+    parsed.contactType = filter.contactType;
+  }
 
   return parsed;
 };
+
+
+
+
+
+// import { parseGender } from './parseGender.js';
+// import { parseBoolean } from './parseBoolean.js';
+// import { parseNumber } from './parseNumber.js';
+// import { GENDERS } from '../constants/gender.js';
+
+// export const parseFilters = (filter = {}) => {
+//   const parsed = {
+//     name: filter.name || '',
+//     phoneNumber: filter.phoneNumber || '',
+//     email: filter.email || '',
+//     isFavourite: filter.isFavourite !== undefined ? Boolean(filter.isFavourite) : false, // если не передано, будет false
+//     contactType: filter.contactType && GENDERS.includes(filter.contactType) ? filter.contactType : GENDERS[0],
+//   };
+
+//   return parsed;
+// };
