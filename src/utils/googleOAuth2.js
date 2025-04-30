ff94f0a5a3bf00d9c0bf1881c03b799d2ef8bb47
PavelSteep <141700339+PavelSteep@users.noreply.github.com>
2025-04-30 22:10:42 +0200

    30.04.25-5

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -1,19 +1,15 @@
 import { OAuth2Client } from 'google-auth-library';
-import path from 'node:path';
-import { readFile } from 'fs/promises';
 import { getEnvVar } from './getEnvVar.js';
 import createHttpError from 'http-errors';
 
-const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
-
-const oauthConfig = JSON.parse(await readFile(PATH_JSON));
-
+// Получаем конфиг из .env
 const googleOAuthClient = new OAuth2Client({
   clientId: getEnvVar('GOOGLE_AUTH_CLIENT_ID'),
   clientSecret: getEnvVar('GOOGLE_AUTH_CLIENT_SECRET'),
-  redirectUri: oauthConfig.web.redirect_uris[0],
+  redirectUri: getEnvVar('GOOGLE_AUTH_REDIRECT_URI'),
 });
 
+// Генерация URL авторизации
 export const generateAuthUrl = () =>
   googleOAuthClient.generateAuthUrl({
     scope: [
@@ -22,23 +18,72 @@ export const generateAuthUrl = () =>
     ],
   });
 
-  export const validateCode = async (code) => {
-    const response = await googleOAuthClient.getToken(code);
-    if (!response.tokens.id_token) throw createHttpError(401, 'Unauthorized');
+// Валидация кода от Google
+export const validateCode = async (code) => {
+  const response = await googleOAuthClient.getToken(code);
+  if (!response.tokens.id_token) throw createHttpError(401, 'Unauthorized');
+
+  const ticket = await googleOAuthClient.verifyIdToken({
+    idToken: response.tokens.id_token,
+  });
+  return ticket;
+};
+
+// Получение полного имени пользователя
+export const getFullNameFromGoogleTokenPayload = (payload) => {
+  let fullName = 'Гость';
+  if (payload.given_name && payload.family_name) {
+    fullName = `${payload.given_name} ${payload.family_name}`;
+  } else if (payload.given_name) {
+    fullName = payload.given_name;
+  }
+  return fullName;
+};
+
+
+
+
+// import { OAuth2Client } from 'google-auth-library';
+// import path from 'node:path';
+// import { readFile } from 'fs/promises';
+// import { getEnvVar } from './getEnvVar.js';
+// import createHttpError from 'http-errors';
+
+// const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
+
+// const oauthConfig = JSON.parse(await readFile(PATH_JSON));
+
+// const googleOAuthClient = new OAuth2Client({
+//   clientId: getEnvVar('GOOGLE_AUTH_CLIENT_ID'),
+//   clientSecret: getEnvVar('GOOGLE_AUTH_CLIENT_SECRET'),
+//   redirectUri: oauthConfig.web.redirect_uris[0],
+// });
+
+// export const generateAuthUrl = () =>
+//   googleOAuthClient.generateAuthUrl({
+//     scope: [
+//       'https://www.googleapis.com/auth/userinfo.email',
+//       'https://www.googleapis.com/auth/userinfo.profile',
+//     ],
+//   });
+
+//   export const validateCode = async (code) => {
+//     const response = await googleOAuthClient.getToken(code);
+//     if (!response.tokens.id_token) throw createHttpError(401, 'Unauthorized');
   
-    const ticket = await googleOAuthClient.verifyIdToken({
-      idToken: response.tokens.id_token,
-    });
-    return ticket;
-  };
+//     const ticket = await googleOAuthClient.verifyIdToken({
+//       idToken: response.tokens.id_token,
+//     });
+//     return ticket;
+//   };
   
-  export const getFullNameFromGoogleTokenPayload = (payload) => {
-    let fullName = 'Гость';
-    if (payload.given_name && payload.family_name) {
-      fullName = `${payload.given_name} ${payload.family_name}`;
-    } else if (payload.given_name) {
-      fullName = payload.given_name;
-    }
+//   export const getFullNameFromGoogleTokenPayload = (payload) => {
+//     let fullName = 'Гость';
+//     if (payload.given_name && payload.family_name) {
+//       fullName = `${payload.given_name} ${payload.family_name}`;
+//     } else if (payload.given_name) {
+//       fullName = payload.given_name;
+//     }
   
-    return fullName;
-  };
+//     return fullName;
+//   };
